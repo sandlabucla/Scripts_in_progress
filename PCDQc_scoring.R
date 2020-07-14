fc391bddcc570f0eb9b2638922787e938ab23de2
yaelwaizman <yaelwaizman00@ucla.edu>
2020-07-13 19:02:52 -0700

    updated PCDQ scoring

---
+++ PCDQc_scoring.R
@@ -0,0 +1,10 @@
+#########################################################################################################################################################
+# Parent-Child Disagreements Questionnaire - Child (PCDQc) Version scoring
+# Created by (insert RA name(s) here)
+#########################################################################################################################################################
+
+PCDQc <- function(dataframe){
+  #note: insert code to score questionnaire here
+  
+  return(dataframe)
+}
---
+++ PCDQp_scoring.R
@@ -0,0 +1,10 @@
+#########################################################################################################################################################
+# Parent-Child Disagreements Questionnaire - Parent (PCDQp) Version scoring
+# Created by (insert RA name(s) here)
+#########################################################################################################################################################
+
+PCDQp <- function(dataframe){
+  #note: insert code to score questionnaire here
+  
+  return(dataframe)
+}
