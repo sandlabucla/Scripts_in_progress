928f784d5c5c81409159af304c33059bd523a620
yaelwaizman <yaelwaizman00@ucla.edu>
2020-07-17 14:02:15 -0700

    created ICc functionto score questionnaire

--- ICc_scoring.R
+++ ICc_scoring.R
@@ -4,7 +4,11 @@
 #########################################################################################################################################################
 
 ICc <- function(dataframe){
-  #note: insert code to score questionnaire here
-  
+  #Quantity of issues: Count the number of issues marked “yes.”
+  dataframe$ICc_Issues_Sum <- rowSums(dataframe[,paste("ICc",c(1:21,"Other"),sep="_")], na.rm = FALSE)
+  dataframe$ICc_Issues_Mean <- rowMeans(dataframe[,paste("ICc",c(1:21,"Other"),sep="_")], na.rm = TRUE)
+  #Intensity of issues: For issues marked “yes,” add intensity ratings and divide by the number of issues marked 
+  #“yes” to obtain mean intensity rating.
+  dataframe$ICc_Mean_Intensity <- rowMeans(dataframe[,paste("ICc",c(1:21,"Other"), "Upset",sep="_")], na.rm = TRUE)
   return(dataframe)
 }
