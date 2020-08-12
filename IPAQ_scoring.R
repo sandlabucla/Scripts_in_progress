33ebc1a860d0d99b1e28f83b91de0c50f41e096c
Aayush Patel <aayushpatel@macbook-pro.lan>
2020-08-11 21:00:00 -0500

    Need to edit code, the output is a little off than expected

--- IPAQ_scoring.R
+++ IPAQ_scoring.R
@@ -3,8 +3,33 @@
 # Created by Aayush Patel
 #########################################################################################################################################################
 
-IPAQ <- function(dataframe){
-  #note: insert code to score questionnaire here
-  
-  return(dataframe)
-}
+dataframe <- read.csv("/Users/aayushpatel/Desktop/DATAIneedtouse.csv", header = TRUE)
+
+finalscore<- c()
+  for(i in 1:length(dataframe$IPAQ_1)) {
+    if(dataframe$IPAQ_1[i] ==1 & dataframe$IPAQ_2 ==1){
+      vigorous_activity <- dataframe$IPAQ_1_1_TEXT[i] * dataframe$IPAQ_2_1_TEXT[i] * 8
+    } else {
+      vigorous_activity <- 0
+    }
+    
+    if(dataframe$IPAQ_3[i] ==1 & dataframe$IPAQ_4[i] ==1){
+      moderate_activity <- dataframe$IPAQ_3_1_TEXT[i] * dataframe$IPAQ_4_1_TEXT[i] * 4
+    } else {
+      moderate_activity <- 0
+    }
+    
+    if(dataframe$IPAQ_5[i] ==1 & dataframe$IPAQ_6[i] ==1){
+      walking_activity <- dataframe$IPAQ_5_1_TEXT[i] * dataframe$IPAQ_6_1_TEXT[i] * 3.3
+    } else {
+      walking_activity <-0
+    }
+    totalscore = vigorous_activity + moderate_activity + walking_activity
+    finalscore = append(finalscore, totalscore)
+  }
+
+finalscore
+
+
+
+
