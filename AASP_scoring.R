a6669f63fd5417794ac67bab33538f49a5279f9c
Joyce Forster <joyceforster@MacBook-Air.local>
2020-08-19 19:04:38 -0700

    Fixed variable names

--- AASP_scoring.R
+++ AASP_scoring.R
@@ -7,16 +7,16 @@ AASP <- function(dataframe){
   ##Calculate raw scores:
   
   #Add up raw scores for Quadrant 1 (Low Registration)
-  dataframe$AASP_RawScore_q1 <- dataframe$AASP_taste_3 + dataframe$AASP_taste_6 + dataframe$AASP_mvmt_4 + dataframe$AASP_mvmt_7 + dataframe$AASP_vis_5 + dataframe$AASP_vis_7 + dataframe$AASP_touch_11 + dataframe$AASP_touch_12 + dataframe$AASP_touch_14 + dataframe$AASP_activity_2 + dataframe$AASP_activity_5 + dataframe$AASP_activity_6 + dataframe$AASP_aud_3 + dataframe$AASP_aud_6 + dataframe$AASP_aud_10
+  dataframe$AASP_RawScore_q1 <- dataframe$AASP_taste_3 + dataframe$AASP_taste_6 + dataframe$AASP_mvmt_4 + dataframe$AASP_mvmt_7 + dataframe$AASP_vis_5 + dataframe$AASP_vis_7 + dataframe$AASP_touch_10 + dataframe$AASP_touch_11 + dataframe$AASP_touch_13 + dataframe$AASP_activity_2 + dataframe$AASP_activity_5 + dataframe$AASP_activity_6 + dataframe$AASP_aud_3 + dataframe$AASP_aud_6 + dataframe$AASP_aud_10
   
   #Add up raw scores for Quadrant 2 (Sensation Seeking)
-  dataframe$AASP_RawScore_q2 <- dataframe$AASP_taste_2 + dataframe$AASP_taste_4 + dataframe$AASP_taste_8 + dataframe$AASP_mvmt_2 + dataframe$AASP_mvmt_6 + dataframe$AASP_vis_1 + dataframe$AASP_vis_3 + dataframe$AASP_touch_2 + dataframe$AASP_touch_5 + dataframe$AASP_touch_7 + dataframe$AASP_activity_1 + dataframe$AASP_activity_3 + dataframe$AASP_activity_8 + dataframe$AASP_aud_1 + dataframe$AASP_aud_9
+  dataframe$AASP_RawScore_q2 <- dataframe$AASP_taste_2 + dataframe$AASP_taste_4 + dataframe$AASP_taste_8 + dataframe$AASP_mvmt_2 + dataframe$AASP_mvmt_6 + dataframe$AASP_vis_1 + dataframe$AASP_vis_3 + dataframe$AASP_touch_2 + dataframe$AASP_touch_4 + dataframe$AASP_touch_6 + dataframe$AASP_activity_1 + dataframe$AASP_activity_3 + dataframe$AASP_activity_8 + dataframe$AASP_aud_1 + dataframe$AASP_aud_9
   
   #Add up raw scores for Quadrant 3 (Sensory Sensitivity)
-  dataframe$AASP_RawScore_q3 <- dataframe$AASP_taste_7 + dataframe$AASP_mvmt_1 + dataframe$AASP_mvmt_5 + dataframe$AASP_mvmt_8 + dataframe$AASP_vis_4 + dataframe$AASP_vis_6 + dataframe$AASP_vis_9 + dataframe$AASP_touch_1 + dataframe$AASP_touch_6 + dataframe$AASP_touch_8 + dataframe$AASP_touch_9 + dataframe$AASP_activity_9 + dataframe$AASP_aud_2 + dataframe$AAP_aud_5 + dataframe$AASP_aud_11
+  dataframe$AASP_RawScore_q3 <- dataframe$AASP_taste_7 + dataframe$AASP_mvmt_1 + dataframe$AASP_mvmt_5 + dataframe$AASP_mvmt_8 + dataframe$AASP_vis_4 + dataframe$AASP_vis_6 + dataframe$AASP_vis_9 + dataframe$AASP_touch_1 + dataframe$AASP_touch_5 + dataframe$AASP_touch_7 + dataframe$AASP_touch_8 + dataframe$AASP_activity_9 + dataframe$AASP_aud_2 + dataframe$AASP_aud_5 + dataframe$AASP_aud_11
   
   #Add up raw scores for Quadrant 4 (Sensation Avoiding)
-  dataframe$AASP_RawScore_q4 <- dataframe$AASP_taste_1 + dataframe$AASP_taste_5 + dataframe$AASP_mvmt_3 + dataframe$AASP_vis_2 + dataframe$AASP_vis_8 + dataframe$AASP_vis_10 + dataframe$AASP_touch_3 + dataframe$AASP_touch_10 + dataframe$AASP_touch_13 + dataframe$AASP_activity_4 + dataframe$AASP_activity_7 + dataframe$AASP_activity_10 + dataframe$AASP_aud_4 + dataframe$AASP_aud_7 + dataframe$AASP_aud_8
+  dataframe$AASP_RawScore_q4 <- dataframe$AASP_taste_1 + dataframe$AASP_taste_5 + dataframe$AASP_mvmt_3 + dataframe$AASP_vis_2 + dataframe$AASP_vis_8 + dataframe$AASP_vis_10 + dataframe$AASP_touch_3 + dataframe$AASP_touch_9 + dataframe$AASP_touch_12 + dataframe$AASP_activity_4 + dataframe$AASP_activity_7 + dataframe$AASP_activity_10 + dataframe$AASP_aud_4 + dataframe$AASP_aud_7 + dataframe$AASP_aud_8
   
   ##Quadrant Profile (based on raw scores):
   #-- = Much Less Than Most People
