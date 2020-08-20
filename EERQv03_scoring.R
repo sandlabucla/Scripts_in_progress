c13324bd640f42abbf1928977621a0665bd4381a
yaelwaizman <yaelwaizman00@ucla.edu>
2020-08-19 21:11:57 -0700

    added Reapp and Supp scoring to the EERQ scoring script

--- EERQv03_scoring.R
+++ EERQv03_scoring.R
@@ -4,7 +4,15 @@
 #########################################################################################################################################################
 
 EERQ <- function(dataframe){
-  #note: insert code to score questionnaire here
+  #calculate Cognitive Reappraisal Mean
+  dataframe$ERQ_Reapp_MEAN <- rowMeans(dataframe[,paste("EERQ",c(1,5,10,17,19,22),sep="")], na.rm = TRUE)
+  #calculate Cognitive Reappraisal Sum Total
+  dataframe$ERQ_Reapp_TOTAL <- rowSums(dataframe[,paste("EERQ",c(1,5,10,17,19,22),sep="")], na.rm = FALSE)
   
+  #calculate Expressive Suppression Mean
+  dataframe$ERQ_Supp_MEAN <- rowMeans(dataframe[,paste("EERQ",c(4,9,13,20),sep="")], na.rm = TRUE)
+  #calculate Expressive Suppression Sum Total
+  dataframe$ERQ_Supp_TOTAL <- rowSums(dataframe[,paste("EERQ",c(4,9,13,20),sep="")], na.rm = FALSE) 
+
   return(dataframe)
 }
